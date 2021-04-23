56a82a44c7be345aec0653eb4087f014228a404d
Jeffroyang <jeffroyang@gmail.com>
2021-04-23 00:51:36 -0400

    Profile update

--- AnimeRecs/src/Profile.java
+++ AnimeRecs/src/Profile.java
@@ -1,4 +1,22 @@
+import java.util.HashMap;
 
 public class Profile {
+	
+	private HashMap<String, Integer> animeList;
+	
+	public Profile() {
+		animeList = new HashMap<String, Integer>();
+	}
+	
+	public void addAnime (String animeName, int score) {
+		if (score < 0 || score > 10) {
+			System.out.println("invalid score");
+			return;
+		}
+		
+		animeList.put(animeName, score);
+	}
+	
+	
 
 }

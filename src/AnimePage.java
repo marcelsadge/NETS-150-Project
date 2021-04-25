7cd5426c33e0de21fed15f58353229650ed1eb47
George Wang <ge0rge@seas.upenn.edu>
2021-04-25 10:14:58 -0400

    add image fetching/caching

--- src/AnimePage.java
+++ src/AnimePage.java
@@ -7,8 +7,6 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.jsoup.Jsoup;
 import org.jsoup.Connection.Response;
